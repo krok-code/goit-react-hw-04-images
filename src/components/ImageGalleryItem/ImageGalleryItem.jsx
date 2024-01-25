d4d672fb2de463f4b92beef6251e26e0ca41487f
krok-code <maks.smolinskyi@gmail.com>
2024-01-25 22:52:15 +0200

    Makes some small changes

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({ webformatURL, largeImageURL, tags }) => {
+const ImageGalleryItem = ({
+  webformatURL,
+  webformatWidth,
+  webformatHeight,
+  largeImageURL,
+  tags,
+}) => {
   return (
     <li className={styles.galleryItem}>
-      <img className={styles.galleryItemImage} src={webformatURL} alt={tags} />
+      <img
+        className={styles.galleryItemImage}
+        src={webformatURL}
+        alt={tags}
+        width={webformatWidth}
+        height={webformatHeight}
+      />
     </li>
   );
 };
 
 ImageGalleryItem.propTypes = {
   webformatURL: PropTypes.string.isRequired,
+  webformatWidth: PropTypes.number,
+  webformatHeight: PropTypes.number,
   largeImageURL: PropTypes.string,
   tags: PropTypes.string,
 };
