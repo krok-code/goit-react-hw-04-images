37f55fab3e5e520fad1ab7ad30a849425245420b
krok-code <maks.smolinskyi@gmail.com>
2024-01-25 23:21:02 +0200

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import styles from './SearchForm.module.css';
 
-export default class SearchForm extends Component {
-  state = {
-    input: '',
+const SearchForm = ({ onSubmit }) => {
+  const [input, setInput] = useState('');
+
+  const handleInputOnChange = e => {
+    setInput(e.currentTarget.value);
   };
 
-  handleInputOnChange = e => {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value.trim() });
-  };
-
-  handleFormOnSubmit = e => {
+  const handleFormOnSubmit = e => {
     e.prevDefault();
-    this.props.onSubmit(this.state.input);
+    onSubmit(input.trim());
   };
 
-  render() {
-    return (
-      <form className={styles.form} onSubmit={this.handleFormOnSubmit}>
-        <button type="submit" className={styles.button}>
-          <svg width="20" height="20" viewBox="0 0 20 20">
-            <path
-              d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
-              stroke="currentColor"
-              fill="none"
-              fillRule="evenodd"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
-          </svg>
-          <span className={styles.buttonLabel}>Search</span>
-        </button>
+  return (
+    <form className={styles.form} onSubmit={handleFormOnSubmit}>
+      <button type="submit" className={styles.button}>
+        <svg width="20" height="20" viewBox="0 0 20 20">
+          <path
+            d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+            stroke="currentColor"
+            fill="none"
+            fillRule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+        <span className={styles.buttonLabel}>Search</span>
+      </button>
 
-        <input
-          className={styles.input}
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          name="input"
-          value={this.state.input}
-          onChange={this.handleInputOnCnange}
-        />
-      </form>
-    );
-  }
-}
+      <input
+        className={styles.input}
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search images and photos"
+        name="input"
+        value={input}
+        onChange={handleInputOnChange}
+      />
+    </form>
+  );
+};
+
+SearchForm.propTypes = { onSubmit: PropTypes.func.isRequired };
+
+export default SearchForm;
